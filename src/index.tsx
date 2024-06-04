392a20d63f453fba5a9ed8990a1d5d500c231adb
Sofia <vdo-sofia@mail.ru>
2024-06-04 15:25:29 +0300

    init: init project, add basic structure

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './app/App';
+
+import '@/app/styles/index.scss';
+
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Контейнер не найден. Не удалось вмонтировать приложение');
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,15 @@
+import classNames from 'classnames';
+
+import cls from './MainPage.module.scss';
+
+interface MainPageProps {
+  className?: string;
+}
+
+export const MainPage = ({ className }: MainPageProps) => {
+  return (
+    <div className={classNames(cls.MainPage, className)}>
+
+    </div>
+  )
+}
